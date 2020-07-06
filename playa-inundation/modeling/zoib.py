0791af3e84abc9679caf6be2279b95ce39f1ab7d
Kylen Solvik <kysolvik@gmail.com>
2020-07-06 13:17:20 -0600

    3d zoib. Decent fast fit

--- playa-inundation/modeling/zoib.py
+++ playa-inundation/modeling/zoib.py
@@ -60,10 +60,10 @@ class ZOIBeta(ExponentialFamily):
 
 def zoib_loss(t, y_true, pad=0.0001, return_mean=True):
     neg_log_probs = -1*ZOIBeta(
-        p=t[:,0],
-        q=t[:,1],
-        concentration1=t[:,2]+pad,
-        concentration0=t[:,3]+pad
+            p=t[:,:, 0],
+        q=t[:,:,1],
+        concentration1=t[:,:,2]+pad,
+        concentration0=t[:,:,3]+pad
     ).log_prob(y_true)
     
     if return_mean:
